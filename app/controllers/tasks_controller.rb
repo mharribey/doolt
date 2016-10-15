0b14debf6e51e6709bf156507e93f6ce4135d5a3
mharribey <mathieuharribey@hotmail.fr>
2016-10-15 16:10:57 +0200

    add Create + edit Index / Tasks controller

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -5,11 +5,27 @@ class TasksController < ApplicationController
 		@tasks = Task.all
 	end
 
+	def show
+
+	end
+
 	def new
 
 	end
 
 	def create
+
+	end
+
+	def update
+
+	end
+
+	def edit
+
+	end
+
+	def destroy
 
 	end
 end
