8d100fba1733141cfe76ba85f33aa605b57053da
mharribey <mathieuharribey@hotmail.fr>
2016-12-16 09:01:45 +0100

    add condition deadline

--- app/models/task.rb
+++ app/models/task.rb
@@ -1,11 +1,11 @@
 class Task < ApplicationRecord
   belongs_to :user
 
-  validate :expiration
+  #validate :expiration
 
-  def expiration
-    if deadline.present? && deadline < Date.today
-      errors.add(:deadline, "Can't be in the past")
-    end
-  end
+  #def expiration
+  #  if deadline.present? && deadline < Date.today
+  #    notice.add(:deadline, "Can't be in the past")
+  #  end
+  #end
 end
